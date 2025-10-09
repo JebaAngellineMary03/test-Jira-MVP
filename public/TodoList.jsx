830e63ae71ad736c5fb7036c9fc781202b0220ea
JebaAngellineMary03 <jeba.m.ihub@snsgroups.com>
2025-10-09 18:45:35 +0530

    Update and rename SimpleButton.jsx to TodoList.jsx

---
+++ public/TodoList.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    if (input.trim() !== "") {
+      setTodos([...todos, input]);
+      setInput("");
+      console.log("Todo added:", input);
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+    console.log("Todo removed at index:", index);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a todo"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo} <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
