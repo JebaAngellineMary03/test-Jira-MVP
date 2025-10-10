ac16643ef1d56598a0ed240886b764fa7bda9afa
JebaAngellineMary03 <jeba.m.ihub@snsgroups.com>
2025-10-10 18:01:07 +0530

    Update TodoList.jsx

--- public/TodoList.jsx
+++ public/TodoList.jsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 export default function TodoList() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
-  console.log("hi")
   const addTodo = () => {
     if (input.trim() !== "") {
       setTodos([...todos, input]);
