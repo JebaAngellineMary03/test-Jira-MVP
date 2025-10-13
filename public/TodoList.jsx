1ad98e5379a0f8ddc79c16554cf24f3317b22e3c
JebaAngellineMary03 <jeba.m.ihub@snsgroups.com>
2025-10-13 10:43:27 +0530

    Update TodoList.jsx

--- public/TodoList.jsx
+++ public/TodoList.jsx
@@ -8,7 +8,7 @@ export default function TodoList() {
     if (input.trim() !== "") {
       setTodos([...todos, input]);
       setInput("");
-      console.log("Todo added", input);
+      console.log("Todo addeddd", input);
     }
   };
 
