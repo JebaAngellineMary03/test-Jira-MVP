8fd468d3b839b3c7394834171f96ce8dfd93ca7d
JebaAngellineMary03 <jeba.m.ihub@snsgroups.com>
2025-11-01 15:16:24 +0530

    HGFSJ

--- public/TodoList.jsx
+++ public/TodoList.jsx
@@ -8,7 +8,7 @@ export default function TodoList() {
     if (input.trim() !== "") {
       setTodos([...todos, input]);
       setInput("");
-      console.log("Todo addffded", input);
+      console.log("Todo addffdHGJed", input);
     }
   };
 
