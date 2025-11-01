5fa3ca75b8990614215b70b4d93b65a9c11f1aed
JebaAngellineMary03 <jeba.m.ihub@snsgroups.com>
2025-11-01 10:38:49 +0530

    Fix typo in console log for added todo

--- public/TodoList.jsx
+++ public/TodoList.jsx
@@ -8,7 +8,7 @@ export default function TodoList() {
     if (input.trim() !== "") {
       setTodos([...todos, input]);
       setInput("");
-      console.log("Todo addde", input);
+      console.log("Todo addded", input);
     }
   };
 
